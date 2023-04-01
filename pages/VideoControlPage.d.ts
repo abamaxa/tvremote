ac3ccb4ec38ff4ca97010ac0b413604feb21ae97
Chris Morgan <cmorgan@abamaxa.com>
2023-04-01 14:10:12 +0100

    removed uneeded files

--- pages/VideoControlPage.d.ts
+++
@@ -1,6 +0,0 @@
-import { RestAdaptor } from "../src/adaptors/RestAdaptor";
-type VideoControlProps = {
-    host: RestAdaptor;
-};
-declare const VideoControlPage: (props: VideoControlProps) => JSX.Element;
-export default VideoControlPage;
